91bb1a9ee54ac90d476d6a93da7c49fdf43bcbe4
FoxLlik <yukionna697@gmail.com>
2024-08-28 14:37:23 +0800

    1

---
+++ .robo/build/app/App.js
@@ -0,0 +1,14 @@
+import { DiscordContextProvider } from "../hooks/useDiscordSdk.js";
+import { Activity } from "./Activity.js";
+import "./App.css";
+/**
+ * 🔒 Set `authenticate` to true to enable Discord authentication
+ * You can also set the `scope` prop to request additional permissions
+ *
+ * Example:
+ * ```tsx
+ * <DiscordContextProvider authenticate scope={['identify', 'guilds']}>
+ * ```
+ */ export default function App() {
+    return /*#__PURE__*/ React.createElement(DiscordContextProvider, null, /*#__PURE__*/ React.createElement(Activity, null));
+}
